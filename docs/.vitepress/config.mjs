0822b905543e5677d661c1b0383e1bc14713da6a
田志会 Chris <chris.tian@biposervice.com>
2023-12-29 13:05:08 +0800

    fix: bug

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: 'vitepress-money',
+  base: '/vitepress-money/',
   title: "Money",
   description: "Money Calculator",
   themeConfig: {
