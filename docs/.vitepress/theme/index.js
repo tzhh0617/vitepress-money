cb3d630ace1fb5bcdfc7d7ce3f14d22a8d30ed72
TZH\zhihui <tzhh0617@gmail.com>
2024-01-02 22:02:26 +0800

    chore

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 import 'virtual:uno.css'
 
 
